cd36f8e7bcf2f291bf8a17b02c981f445653a847
sbahlool <saud.bahlool@hotmail.com>
2023-11-08 12:29:15 +0300

    added home and about views

--- Predictor/settings.py
+++ Predictor/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv('DATABASENAME'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +81,9 @@ WSGI_APPLICATION = 'Predictor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': os.getenv('DATABASEUSER'),
+        'NAME': os.getenv('DATABASENAME'),
     }
 }
 
